110c242e58f5054f0d3ae2ff7c73d542fd5ec97a
mike <michaelfromano@gmail.com>
2020-09-19 15:41:32 -0400

    added models from chesterai

--- public/javascripts/predict.js
+++ public/javascripts/predict.js
@@ -1,14 +1,13 @@
 var body = document.getElementById('main');
 var para = document.createElement('p');
+
 body.appendChild(para);
-tens = tf.tensor([1, 2, 3, 4])
-tens.data().then(data => para.appendChild(document.createTextNode(data)));
 
-
-MODEL_PATH = 'https://mlmed.github.io/tools/xray/models/xrv-all-45rot15trans15scale';
-AEMODEL_PATH = 'https://mlmed.github.io/tools/xray/models/ae-chest-savedmodel-64-512';
+AEMODEL_PATH = 'file:///home/mfromano/Research/pathray/pathray/models/ae-chest-savedmodel-64-512';
 
 async function loadModel(path) {
     var model = await tf.loadLayersModel(path);
     return model
 }
+const model = loadModel(AEMODEL_PATH)
+model.then(data => para.appendChild(document.createTextNode(data)))
