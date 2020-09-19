50c8e75dea0f07d26b548d407255548e4d1d83c6
mike <michaelfromano@gmail.com>
2020-09-19 15:17:24 -0400

    added onnx, tf.js

--- public/javascripts/predict.js
+++ public/javascripts/predict.js
@@ -3,3 +3,12 @@ var para = document.createElement('p');
 body.appendChild(para);
 tens = tf.tensor([1, 2, 3, 4])
 tens.data().then(data => para.appendChild(document.createTextNode(data)));
+
+
+MODEL_PATH = 'https://mlmed.github.io/tools/xray/models/xrv-all-45rot15trans15scale';
+AEMODEL_PATH = 'https://mlmed.github.io/tools/xray/models/ae-chest-savedmodel-64-512';
+
+async function loadModel(path) {
+    var model = await tf.loadLayersModel(path);
+    return model
+}
