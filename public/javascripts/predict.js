deb88da78a122fff1ce5f431b04de58bb35d473e
Michael Romano <michaelfromano@gmail.com>
2020-10-21 16:10:10 -0400

    updated predict.js

--- public/javascripts/predict.js
+++ public/javascripts/predict.js
@@ -6,26 +6,21 @@ var ctx=canvas.getContext("2d");
 
 MODEL_PATH = 'https://mlmed.github.io/tools/xray/models/xrv-all-45rot15trans15scale/';
 
-
-async function loadModel(path) {
-    return tf.loadGraphModel(path + 'model.json');
-}
-
-async function resizeImage(img) {
-    var imgCropped = await config.then(data => prepare_image_resize_crop(img, data.IMAGE_SIZE));
-    return config.then(data => imgCropped.mul(2).sub(1).mul(tf.scalar(data.IMAGE_SCALE)));
+async function resizeImage(img, config) {
+    let imgCropped = await prepare_image_resize_crop(img, config.IMAGE_SIZE);
+    return imgCropped.mul(2).sub(1).mul(tf.scalar(config.IMAGE_SCALE));
 }
 
 async function featureScale(arr) {
-    var max = await Math.max.apply(null, arr);
-    var min = await Math.min.apply(null, arr);
+    let max = await Math.max.apply(null, arr);
+    let min = await Math.min.apply(null, arr);
     return arr.map(x => (x-min)*255/(max-min));
 }
 
 function expandArray(arr) {
-    var newArr = new Uint8ClampedArray(arr.length*4);
-    var newArrInd = 0;
-    for (var i=0; i<arr.length; i++) {
+    let newArr = new Uint8ClampedArray(arr.length*4);
+    let newArrInd = 0;
+    for (let i=0; i<arr.length; i++) {
         newArr[newArrInd] = arr[i];
         newArr[newArrInd+1] = arr[i];
         newArr[newArrInd+2] = arr[i];
@@ -36,31 +31,39 @@ function expandArray(arr) {
 }
 
 async function makeImageNode(im_age) {
-    canvas.width = await im_age.then(res => res.shape[0]);
-    canvas.height = await im_age.then(res => res.shape[1]);
-    var rescaled = await im_age.then(res => featureScale(res.dataSync()));
+    canvas.width = im_age.shape[0];
+    canvas.height = im_age.shape[1];
+    var rescaled = await featureScale(im_age.dataSync());
     var arr = new Uint8ClampedArray(rescaled);
     arr = expandArray(arr);
-    const imdat = new ImageData(arr, canvas.width, canvas.height);
-    ctx.putImageData(imdat, 0, 0);
+    const imageData = new ImageData(arr, canvas.width, canvas.height);
+    ctx.putImageData(imageData, 0, 0);
+}
+
+async function run(im_g) {
+    let config = await $.getJSON(MODEL_PATH + 'config.json')
+    let model = await tf.loadGraphModel(MODEL_PATH + 'model.json');
+    let imgResized = await resizeImage(im_g, config);
+    await makeImageNode(imgResized);
+    await predictAndPlot(model, imgResized, config);
 }
 
 async function predict(model, croppedImage) {
     croppedImage = croppedImage.reshape([1,1,croppedImage.shape[0], croppedImage.shape[1]])
-    return await model.then(res => res.predict(croppedImage));
+    return model.predict(croppedImage);
 }
 
-async function appendTableDom(results) {
-    var labels = await config.then(res => res.LABELS);
-    var tbl = document.createElement('table');
+function appendTableDom(results, config) {
+    const labels = config.LABELS;
+    const tbl = document.createElement('table');
     tbl.setAttribute('id', 'resultsTable')
-    await tbl.createTHead().insertRow().appendChild(document.createElement('th')
+    tbl.createTHead().insertRow().appendChild(document.createElement('th')
         .appendChild(document.createTextNode('Results')));
-    var tblBody = await tbl.createTBody()
+    const tblBody = tbl.createTBody()
     for (var i=0; i < labels.length; i++) {
         var row = tblBody.insertRow();
-        await row.insertCell().appendChild(document.createTextNode(labels[i]));
-        await results.then(res => row.insertCell().appendChild(document.createTextNode(res[i]*100)));
+        row.insertCell().appendChild(document.createTextNode(labels[i]));
+        row.insertCell().appendChild(document.createTextNode(results[i]*100));
     }
     body.appendChild(tbl);
 }
@@ -69,23 +72,11 @@ async function applyClassActivationMap(mod, cla_ss=2, img) {
     return gradClassActivationMap(mod, cla_ss, img);
 }
 
-async function predictAndPlot(mod, im_age) {
-    let pred = await im_age.then(res => predict(mod, res));
-    im_age = await im_age.then(res => res.reshape([1,1,res.shape[0], res.shape[1]]))
-    await appendTableDom(pred.data());
-    var maxClass = await pred.dataSync().indexOf(Math.max(...pred.dataSync()));
-    return mod.then(mdl => computeGrads_real(mdl, im_age, maxClass));
+async function predictAndPlot(mod, im_age, config) {
+    let pred = await predict(mod, im_age);
+    var data = await pred.data()
+    appendTableDom(data, config);
+    let maxClass = pred.dataSync().indexOf(Math.max(...pred.dataSync()));
+    im_age = im_age.reshape([1,1,im_age.shape[0], im_age.shape[1]])
+    return await computeGrads_real(mod, im_age, maxClass);
 }
-
-
-var config = $.getJSON(MODEL_PATH + 'config.json', function (json) {
-    return json;
-});
-
-const model = loadModel(MODEL_PATH);
-
-// let img = new Image();
-// img.src = 'atelectasis.jpeg';
-// var imgResized = resizeImage(img);
-// makeImageNode(imgResized);
-// predictAndPlot(model, imgResized);
